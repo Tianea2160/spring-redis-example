5401a4f2590bb154325d8ceece38fc7b73cb3c0d
johyeonjun <rhlehfndvkd7557@daum.net>
2025-05-10 05:19:56 +0900

    exception에 대한 실행 테스트

--- redis-distributed-lock/src/test/kotlin/org/tianea/redisdistributedlock/aop/DistributedLockAspectIntegrationTest.kt
+++ redis-distributed-lock/src/test/kotlin/org/tianea/redisdistributedlock/aop/DistributedLockAspectIntegrationTest.kt
@@ -13,6 +13,7 @@ import org.tianea.redisdistributedlock.aop.LockFailureStrategy.EXECUTE_FALLBACK
 import org.tianea.redisdistributedlock.exception.DistributedLockException
 import java.lang.Thread.sleep
 import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeUnit.*
 import kotlin.concurrent.thread
 import kotlin.time.Duration
 import kotlin.time.measureTime
@@ -80,18 +81,18 @@ class DistributedLockAspectIntegrationTest
 class TestService {
     private val logger = LoggerFactory.getLogger(TestService::class.java)
 
-    @DistributedLock(key = "#param1", leaseTime = 1L, timeUnit = TimeUnit.SECONDS)
+    @DistributedLock(key = "#param1", leaseTime = 1L, timeUnit = SECONDS)
     fun test(param1: String) {
         logger.info("param1: $param1")
     }
 
-    @DistributedLock(key = "#param1", leaseTime = 3L, waitTime = 0L, timeUnit = TimeUnit.SECONDS)
+    @DistributedLock(key = "#param1", leaseTime = 3L, waitTime = 0L, timeUnit = SECONDS)
     fun testWithSleep(param1: String) {
         logger.info("test with sleep, param1: $param1")
         sleep(1000)
     }
 
-    @DistributedLock(key = "#param1", leaseTime = 1L, waitTime = 1L, timeUnit = TimeUnit.SECONDS)
+    @DistributedLock(key = "#param1", leaseTime = 1L, waitTime = 1L, timeUnit = SECONDS)
     fun testWithWaitingTime(param1: String) {
         logger.info("test with waiting param1: $param1")
     }
@@ -100,7 +101,7 @@ class TestService {
         key = "#param1",
         waitTime = 0L,
         leaseTime = 2L,
-        timeUnit = TimeUnit.SECONDS,
+        timeUnit = SECONDS,
         failureStrategy = EXECUTE_FALLBACK,
         fallbackMethod = "fallback"
     )
