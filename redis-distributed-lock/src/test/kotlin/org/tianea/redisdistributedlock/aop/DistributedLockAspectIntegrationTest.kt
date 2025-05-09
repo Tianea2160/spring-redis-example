df4aaee6b43d5b7967dbcabae463e299cea22eca
johyeonjun <rhlehfndvkd7557@daum.net>
2025-05-10 04:50:50 +0900

    다른 스레드에서 메소드 실행 테스트 추가

--- redis-distributed-lock/src/test/kotlin/org/tianea/redisdistributedlock/aop/DistributedLockAspectIntegrationTest.kt
+++ redis-distributed-lock/src/test/kotlin/org/tianea/redisdistributedlock/aop/DistributedLockAspectIntegrationTest.kt
@@ -1,13 +1,17 @@
 package org.tianea.redisdistributedlock.aop
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.stereotype.Component
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.tianea.redisdistributedlock.exception.DistributedLockException
+import java.lang.Thread.sleep
 import java.util.concurrent.TimeUnit
+import kotlin.concurrent.thread
 
 @SpringBootTest
 @ExtendWith(SpringExtension::class)
@@ -21,6 +25,18 @@ class DistributedLockAspectIntegrationTest
         testService.test("hello world")
     }
 
+    @Test
+    fun `when other thread call current method, this thread can not call same method`() {
+        thread {
+            testService.testWithSleep("hello world")
+        }
+
+        sleep(100)
+
+        assertThrows<DistributedLockException> {
+            testService.testWithSleep("hello world")
+        }
+    }
 }
 
 
@@ -31,5 +47,11 @@ class TestService {
     @DistributedLock(key = "#param1", leaseTime = 1L, timeUnit = TimeUnit.SECONDS)
     fun test(param1: String) {
         logger.info("param1: $param1")
+    }
+
+    @DistributedLock(key = "#param1", leaseTime = 3L, waitTime = 0L, timeUnit = TimeUnit.SECONDS)
+    fun testWithSleep(param1: String) {
+        logger.info("test with sleep, param1: $param1")
+        sleep(1000)
     }
 }
